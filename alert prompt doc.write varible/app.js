552bafedb61dc09bfc24615b368f629dc4ffc44c
Samreen Fatima <samreen11fatima22@gmail.com>
2024-11-22 02:54:09 +0500

    Add files via upload
    js first class

---
+++ alert prompt doc.write varible/app.js
@@ -0,0 +1,29 @@
+// alert("introduction to javaScript")
+// //function : built in and user defind function 
+// // data types: int float string 
+
+// alert(123)
+// alert('hello world')
+
+// var abc = 'Samreen Fatima'
+// alert(abc)
+
+// document.write(' <h1>this is js first class </h1>')//to write on the document page 
+
+// document.write('<h1>'+ 'hello world'+'</h1>')// second way to write hello world in h1
+// var firstName='samreen'
+// var lastName='fatima'
+// //+ tab concat  kre ga jab strig hogi or + wli functionality string m ni hogi 
+// var num1='9'
+// var num2=10
+// document.write(num1+num2)
+
+
+// document.write(firstName+' '+lastName)
+
+//  jo value hm prompt ko de ga osko document p print krni ha
+
+var a=prompt('enter value 1')
+var b=prompt('enter value 2')
+
+document.write(a%b)
